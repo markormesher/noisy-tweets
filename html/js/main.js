6bde33f7822b05d7eccd60b531684c700073efc7
Fares Alaboud <fares.alaboud@gmail.com>
2014-10-04 18:17:04 +0100

    removed labels from graph, tweets now can be added to a table

--- html/js/main.js
+++ html/js/main.js
@@ -1,31 +1,25 @@
 
+
+var graphLabels = [""]
+
 // GRAPH
 
 // Get the context of the canvas element we want to select
 var ctx = document.getElementById("moodChart").getContext("2d");
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["", "", "", "", "", "", ""],
     datasets: [
         {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
+            label: "Live Mood Dataset",
+            fillColor: "rgba(20,150,220,0.2)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
             pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
+            pointHighlightFill: "#ffaaaa",
             pointHighlightStroke: "rgba(220,220,220,1)",
+            // TODO: Assign array of data here
             data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
         }
     ]
 };
@@ -35,5 +29,39 @@ var myLineChart = new Chart(ctx).Line(data, {
 });
 
 redraw = function() {
-    $("#moodChart").html("<canvas id=\"canvas\" height=\"400\" width=\"710\"></canvas>");
+    var ctx = document.getElementById("moodChart").getContext("2d");
+
+    var data = {
+        // TODO: Assign graphLabels to this
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "Live Mood Dataset",
+                fillColor: "rgba(20,150,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#ffaaaa",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
+        ]
+    };
+
+    var myLineChart = new Chart(ctx).Line(data, {
+        showScale: true
+    });
+    
+    $("#moodChart").html("<canvas id=\"moodChart\" height=\"400\" width=\"710\"></canvas>");
+}
+
+addTweets = function(tweet_message, mood) {
+    
+    if(mood < -50) {
+        $( "tbody" ).append( "<tr class=\"danger\"><td>" + tweet_message + "</td><td>" + mood + "</td></tr>" );   
+    } else if (mood > 50) {
+        $( "tbody" ).append( "<tr class=\"success\"><td>" + tweet_message + "</td><td>" + mood + "</td></tr>" );   
+    } else {
+        $( "tbody" ).append( "<tr><td>" + tweet_message + "</td><td>" + mood + "</td></tr>" );   
+    }
 }
